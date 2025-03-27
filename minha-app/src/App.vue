a5283eeae5789af30711545f78697183ad6921e6
riccileonardo <l.lopesricci@gmail.com>
2025-03-26 22:16:12 -0400

    first commit

---
+++ minha-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1> Olá {{nome}}, você tem {{ new Date().getFullYear() - anoNascimento }} anos!</h1>
+    <br>
+    <h2 style="color: {{color}}">Fonte da Juventude:</h2>
+    <!--  V-ON liga o template com o script. -->
+    <button @click="rejuvenescer">Clique Aqui</button>
+    <input type="text" v-model="nome">  
+
+</template>
+
+<script setup>
+
+import { ref } from 'vue';
+
+const nome = ref('Leozin');
+const anoNascimento = ref(2003);
+const rejuvenescer = () => {
+    anoNascimento.value += 10;
+}
+const color = ref('red');
+</script>
+
+<style>
+    p{ color: red;}
+    h1{ color: blue;}
+</style>
